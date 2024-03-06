1f7857fd62164e9525784a2c3db58e55e792fe0e
Sheza Shamsher <160360815+ShezaShamsher@users.noreply.github.com>
2024-03-06 13:05:16 -0800

    Add files via upload

---
+++ unary_operators/index.ts
@@ -0,0 +1,25 @@
+// There are two types of increment 
+// 1.pre-increment
+// 2.post-increment
+
+// Pre Increment 
+let a : number = 20 ;
+console.log(++a);
+
+console.log(a = a + 5); 
+
+let a1 : number = 16 ;
+console.log(--a1);
+
+
+// Post Increment 
+let b : number = 30 ;
+console.log(b++);
+console.log(b);
+console.log(b++);
+console.log(b);
+
+let b1 : number = 10 ;
+console.log(b1--);
+console.log(b1);
+
